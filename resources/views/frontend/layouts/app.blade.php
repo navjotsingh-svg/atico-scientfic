3a3218d2e4bdcd7a567c097172ec2fcd65520783
Navjot <navjot.singh@thegirafe.in>
2026-08-06 10:43:28 +0530

    update

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -221,7 +221,19 @@ li.li-icon.inter { font-size: 13px; line-height: 30px; }
   <!--   <div class="scroll-to-top">
       <i class="fa fa-angle-double-up" aria-hidden="true"></i>
     </div> -->
-   
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/665438669a809f19fb3576de/1husfok80';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> -->
     
     
